cb72e6bcfd820a92c5d70741928df510d9a9e7a5
nararana <narayana.kri>
2019-01-22 23:05:24 -0600

    DummyPrg

---
+++ src/com/narayana/java/Basic.java
@@ -0,0 +1,10 @@
+package com.narayana.java;
+
+public class Basic
+{
+   public static void main(String args[])   //function or method in java
+   {
+      System.out.println("Hello world");
+   }
+}
+
